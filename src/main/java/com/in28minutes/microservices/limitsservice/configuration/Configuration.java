7c341ea26e1d87acdc1a38919f3e85be11d2a095
Max Gomes <maximiliano.pinto@klarna.com>
2022-08-01 16:14:34 +0200

    Class 128

--- src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "limits-service")
+@ConfigurationProperties("limits-service")
 public class Configuration {
     private int minimum;
     private int maximum;
